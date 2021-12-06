8d1aaba67fc67bd0a5874b2229640c8b60291c48
Danil Ryumin <ryumin_danil@icloud.com>
2021-12-06 17:39:09 +0300

    Initial Commit

---
+++ miniGame2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniGame2
+//
+//  Created by Danil Ryumin on 06.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
